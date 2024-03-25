f21446c486175721f3299dfdda9536fce4335071
PTKu <61538034+PTKu@users.noreply.github.com>
2024-03-25 13:22:24 +0100

    sep 3.1 and 2.9

--- axpansion/twin/Entry.cs
+++ axpansion/twin/Entry.cs
@@ -1,4 +1,4 @@
-﻿#define WEBAPI_3_1
+﻿// #define WEBAPI_3_1
 // ixsharpblazor
 // Copyright (c) 2023 Peter Kurhajec (PTKu), MTS,  and Contributors. All Rights Reserved.
 // Contributors: https://github.com/ix-ax/axsharp/graphs/contributors
@@ -19,14 +19,16 @@ namespace axosimple
     public static class Entry
     {
         public static string TargetIp { get; } = Environment.GetEnvironmentVariable("AXTARGET"); // <- replace by your IP 
-        private const string UserName = "Everybody"; //<- replace by user name you have set up in your WebAPI settings
-        
+
+
 #if WEBAPI_3_1
         private static string Pass => Environment.GetEnvironmentVariable("AX_TARGET_PWD") ?? string.Empty; // <- Pass in the password that you have set up for the user. NOT AS PLAIN TEXT! Use user secrets instead.
-#else
+        private const string UserName = "Anonymous"; //<- replace by user name you have set up in your WebAPI settings
+# else
         private static string Pass => string.Empty; // <- Pass in the password that you have set up for the user. NOT AS PLAIN TEXT! Use user secrets instead.
+        private const string UserName = "Everybody"; //<- replace by user name you have set up in your WebAPI settings
 #endif
-        
+
         private const bool IgnoreSslErrors = true; // <- When you have your certificates in order set this to false.
 
         // public static axosimpleTwinController Plc { get; } 
@@ -36,7 +38,7 @@ namespace axosimple
         
         public static axosimpleTwinController Plc { get; } 
             = new (ConnectorAdapterBuilder.Build()
-                .CreateWebApi(TargetIp, "Anonymous", string.Empty, IgnoreSslErrors));
+                .CreateWebApi(TargetIp, UserName, string.Empty, IgnoreSslErrors));
     }
     
 }
